71e7c2d6054988a9f8a2964fc797ed42930e7115
josmorava <josemorales-1234@hotmail.es>
2020-09-11 18:46:10 -0400

    Clase 3, numeros

---
+++ clase3_numeros.js
@@ -0,0 +1,16 @@
+var edad = 20;
+//edad = edad + 1
+edad +=1 // va a incrementar en 1
+
+var peso = 60 
+//peso = peso - 2
+peso -= 2//Resta en 2
+
+var burger = 2
+peso = peso + burger
+
+var precioVino = 200.3
+var total = Math.round(precioVino*100 * 3) / 100//Funcion para redondear
+var totalStr = total.toFixed(3)//Mostrar en patalla el nmr de decimales, tomar en cuenta dentro de los parametros la cantidad de decimales. MUESTRA EL RESULATDO EN STRINGS
+
+var total2 = parseFloat(totalStr)//transforma a elementos numericos, decimales
