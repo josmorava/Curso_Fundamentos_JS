b8464b9c36cd0bf8de59d928b8dd7d5ce125389a
josmorava <josemorales-1234@hotmail.es>
2020-09-11 18:58:14 -0400

    Clase 4 , funciones

---
+++ clase4_funciones.js
@@ -0,0 +1,8 @@
+//Nombre y edad definidas en clase 1
+var nombre = "Jose", edad = 20
+function imprimirEdad (nombre,edad){
+    console.log(`${nombre} tiene ${edad} años`)
+}
+imprimirEdad("Maria", 23)
+/*imprimirEdad("Alejo", 22)
+imprimirEdad("Mario", 28)*/
