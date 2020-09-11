a293a937411008b4c71aa8768c06d03ad0c2a6c4
josmorava <josemorales-1234@hotmail.es>
2020-09-11 17:33:50 -0400

    Clase 1, variables

---
+++ clase1_variables.js
@@ -0,0 +1,9 @@
+//console.log("hola mundo js")
+
+var nombre = "Jose Silverio" // declarando y asignando valor a la variable
+var apellido = "Morales Varon"
+var edad = 20;
+console.log(`Hola ${nombre +" "+ apellido}`)
+console.log ("Hola " + nombre + " " + apellido)
+console.log("Tengo "+ edad + " años")
+//Lenguaje debilmente tipado
